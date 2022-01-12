f3736b65923399a2815ef082876a30d377ddbca9
yxw <yanxuwei666@163.om>
2022-01-12 21:43:46 +0800

    无

---
+++ yygh-parent/service/service-hosp/src/test/java/com/xuwei/yygh/TestController.java
@@ -0,0 +1,12 @@
+package com.xuwei.yygh;
+
+/**
+ * @Description TODO
+ * @Date 2022/1/12 21:41
+ * @Author yxw
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
